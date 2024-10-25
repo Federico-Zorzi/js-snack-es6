b3d4f009b9c680892a55fb4f840222f043cc74a5
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-10-25 15:23:47 +0200

    snack 1 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-console.log("JS OK");
+// ! SNACK 1
+let weightHeavierBicycles = 0;
+let nameHeavierBicycles = "";
+
+const bicycles = [
+  {
+    name: "Bergamont",
+    weight: 10,
+  },
+  {
+    name: "Bianchi",
+    weight: 40,
+  },
+  {
+    name: "BMC",
+    weight: 20,
+  },
+  {
+    name: "Cannondale",
+    weight: 5,
+  },
+];
+
+for (let bike of bicycles) {
+  if (bike.weight > weightHeavierBicycles) {
+    weightHeavierBicycles = bike.weight;
+    nameHeavierBicycles = bike.name;
+  }
+}
+
+console.log(
+  `La bicicletta più pesante è la ${nameHeavierBicycles} con ${weightHeavierBicycles}kg`
+);
